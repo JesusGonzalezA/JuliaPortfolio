9d020be2933b62e3866ad8dd2271f6510f347e42
Jesús González Álvarez <51116828+JesusGonzalezA@users.noreply.github.com>
2023-08-31 17:09:37 +0200

    Add seo

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,6 +12,43 @@ export default function RootLayout({ children }) {
       <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="title" content="Julia Garbin Portfolio" />
+        <meta
+          name="description"
+          content="Digital Marketing | Ad & Public relationship | Social Media"
+        />
+
+        {/* <!-- Open Graph / Facebook --> */}
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:url"
+          content="https://julia-garbin-lourido.vercel.app/"
+        />
+        <meta property="og:title" content="Julia Garbin Portfolio" />
+        <meta
+          property="og:description"
+          content="Digital Marketing | Ad & Public relationship | Social Media"
+        />
+        <meta
+          property="og:image"
+          content="https://julia-garbin-lourido.vercel.app/_next/image?url=%2Fassets%2Flogo.png&w=3840&q=75"
+        />
+
+        {/* <!-- Twitter --> */}
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta
+          property="twitter:url"
+          content="https://julia-garbin-lourido.vercel.app/"
+        />
+        <meta property="twitter:title" content="Julia Garbin Portfolio" />
+        <meta
+          property="twitter:description"
+          content="Digital Marketing | Ad & Public relationship | Social Media"
+        />
+        <meta
+          property="twitter:image"
+          content="https://julia-garbin-lourido.vercel.app/_next/image?url=%2Fassets%2Flogo.png&w=3840&q=75"
+        />
         <link rel="icon" hreaf="./favicon.ico" />
       </head>
       <body>{children}</body>
