7ed64a2cdb3ef297291aebfc3355005e13d5134e
Jesús González Álvarez <51116828+JesusGonzalezA@users.noreply.github.com>
2023-09-02 19:06:28 +0200

    Fix skills

--- src/app/components/skillset/SkillSet.jsx
+++ src/app/components/skillset/SkillSet.jsx
@@ -1,19 +1,11 @@
 "use client";
 import * as React from 'react';
 import styles from "./skillset.module.css";
-import { useInView } from 'framer-motion';
 
 export default function SkillSet() {
-  const ref = React.useRef();
-  const isInView = useInView(ref, { once: true });
-
   return (
     <>
-      <h2 ref={ref} style={{
-                transform: isInView ? "none" : "translateX(-200px)",
-                opacity: isInView ? 1 : 0,
-                transition: "all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s"
-      }}>Skills</h2>
+      <h2>Skills</h2>
       <div>
           <ul className={styles.skillset}>
               <li>Strategy execution</li>
