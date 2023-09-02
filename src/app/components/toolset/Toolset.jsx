b86aca10325542c153774a69114a6c3605a872ce
Jesús González Álvarez <51116828+JesusGonzalezA@users.noreply.github.com>
2023-09-02 13:07:08 +0200

    Remove header transition

--- src/app/components/toolset/Toolset.jsx
+++ src/app/components/toolset/Toolset.jsx
@@ -2,19 +2,11 @@
 import * as React from 'react';
 import Image from 'next/image';
 import styles from "./toolset.module.css";
-import { useInView } from 'framer-motion';
 
 export default function Toolset() {
-    const ref = React.useRef();
-    const isInView = useInView(ref, { once: true });
-
     return (
         <>
-            <h2 ref={ref} style={{
-                    transform: isInView ? "none" : "translateX(-200px)",
-                    opacity: isInView ? 1 : 0,
-                    transition: "all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s"
-                }}>Tools</h2>
+            <h2>Tools</h2>
             <div>
                 <ul className={styles.skillset}>
                     <li title="Canva">
